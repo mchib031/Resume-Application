3456d069a8674b8363f9ce0422616badbfd73912
Mahdi Chiboub <71799789+mchib031@users.noreply.github.com>
2023-04-13 17:00:48 -0400

    move app component files to Components directory

--- RESUME-APP/src/app/app.module.ts
+++ RESUME-APP/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './Components/app.component';
 import { HomeComponent } from './Components/home/home.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { ResumeComponent } from './Components/resume/resume.component';
